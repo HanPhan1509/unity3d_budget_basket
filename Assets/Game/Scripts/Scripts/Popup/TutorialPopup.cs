3f623636581a5f78f74c23791cd27808b0ff36e7
Điêu <hanptb@kimgoc.com>
2026-03-22 23:26:06 +0700

    tutorial

--- Assets/Game/Scripts/Scripts/Popup/TutorialPopup.cs
+++ Assets/Game/Scripts/Scripts/Popup/TutorialPopup.cs
@@ -1,5 +1,42 @@
 using GreiB.UIManager.Scripts.UIPopup;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialPopup : UIPopup
 {
+    [SerializeField] private Text txt;
+    [SerializeField] private GameObject[] _tutors = new GameObject[4];
+    private int index = 0;
+
+    protected override void OnShowing()
+    {
+        base.OnShowing();
+        txt.text = "Next";
+        index = 0;
+        Show();
+    }
+
+    public void Next()
+    {
+        index++;
+        if (index >= _tutors.Length)
+        {
+            this.Hide();
+            return;
+        }
+        if (index == _tutors.Length - 1)
+        {
+            txt.text = "Close";
+        }
+        Show();
+    }
+
+    private void Show()
+    {
+        for (int i = 0; i < _tutors.Length; i++)
+        {
+            _tutors[i].SetActive(i == index);
+        }
+    }
 }
